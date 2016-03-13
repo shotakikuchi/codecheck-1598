cd22d24640ec2fef506d1b3df569a078f0a952d1
u35118 <u35118@c5c17dff-b264-479d-948e-1c8e8673b72e.prvt.dyno.rt.heroku.com>
2016-03-13 18:42:02 +0000

    Initial commit

---
+++ app/hello.js
@@ -0,0 +1,12 @@
+function hello(name) {
+/*
+* ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
+* nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
+*/
+  //ここにHelloworldを実装します。
+  //return "Hello World!";
+  this.name = name;
+  return "Hello "+this.name + "!";
+}
+
+module.exports = hello;
